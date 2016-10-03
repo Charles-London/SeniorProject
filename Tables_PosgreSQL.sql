248a9e4cefb018e2b2b2e839ce877f78654d264e
Kyle Rapps <rappsk0@students.rowan.edu>
2016-10-03 13:54:24 -0400

    Posgres Syntax
    Originally written MySQL format, Updated to Posgres

--- Tables_PosgreSQL.sql
+++ Tables_PosgreSQL.sql
@@ -12,7 +12,7 @@ PRIMARY KEY (Baker_ID)
 
 CREATE TABLE Bakery(
 Bakery_ID SERIAL NOT NULL,
-Baker_ID int NOT NULL,
+Baker_ID int NOT NULL REFERENCES BAKER(Baker_ID),
 Bakery_Name varchar(30) NOT NULL,
 Bakery_Address varchar(30) NOT NULL,
 Bakery_Offline boolean NOT NULL,
@@ -20,27 +20,24 @@ Close_Time varchar(10) NOT NULL,
 Open_Time varchar(10) NOT NULL,
 Delivery int NOT NULL,
 Phone_Number varchar(20) NOT NULL,
-PRIMARY KEY (Bakery_ID),
-FOREIGN KEY (Baker_ID) REFERENCES Baker(Baker_ID) ON DELETE CASCADE
+PRIMARY KEY (Bakery_ID)
 )
 
 CREATE TABLE Product(
 Product_ID SERIAL NOT NULL,
-Bakery_ID int NOT NULL,
+Bakery_ID int NOT NULL REFERENCES Bakery(Bakery_ID),
 Pname varchar(30) NOT NULL,
 Price varchar(10) NOT NULL,
 Picture varchar(30) NOT NULL,
 Description varchar(140) NOT NULL, --Length of a tweet sufficient for description
 Product_Type varchar(10) NOT NULL,
-PRIMARY KEY (Product_ID),
-FOREIGN KEY (Bakery_ID) REFERENCES Bakery(Bakery_ID) ON DELETE CASCADE
+PRIMARY KEY (Product_ID)
 )
 
 CREATE TABLE Allergens(
-Product_ID SERIAL NOT NULL,
+Product_ID SERIAL NOT NULL REFERENCES Product(Product_ID),
 Allergy_Type varchar(30) NOT NULL,
-PRIMARY KEY (Product_ID, Allergy_Type),
-FOREIGN KEY (Product_ID) REFERENCES Product(Product_ID) ON DELETE CASCADE
+PRIMARY KEY (Product_ID, Allergy_Type)
 )
 
 CREATE TABLE Users(
@@ -56,41 +53,34 @@ PRIMARY KEY (User_ID)
 )
 
 CREATE TABLE BakeryFeedback(
-Bakery_ID SERIAL NOT NULL,
-User_ID int NOT NULL,
+Bakery_ID SERIAL NOT NULL REFERENCES Bakery(Bakery_ID),
+User_ID int NOT NULL REFERENCES Users(User_ID),
 Text varchar(140) NOT NULL,
 Feedback_Date DATE NOT NULL,
-PRIMARY KEY (Bakery_ID, User_ID),
-FOREIGN KEY (Bakery_ID) REFERENCES Bakery(Bakery_ID) ON DELETE CASCADE,
-FOREIGN KEY (User_ID) REFERENCES Users(User_ID) ON DELETE CASCADE
+PRIMARY KEY (Bakery_ID, User_ID)
 )
 
 CREATE TABLE Rating(
-Product_ID SERIAL NOT NULL,
-User_ID int NOT NULL,
+Product_ID SERIAL NOT NULL REFERENCES Product(Product_ID),
+User_ID int NOT NULL REFERENCES Users(User_ID),
 Text varchar(140),
 Rating_Value int NOT NULL,
 Rating_Date DATE NOT NULL,
-PRIMARY KEY (Product_ID, User_ID),
-FOREIGN KEY (Product_ID) REFERENCES Product(Product_ID) ON DELETE CASCADE,
-FOREIGN KEY (User_ID) REFERENCES Users(User_ID) ON DELETE CASCADE
+PRIMARY KEY (Product_ID, User_ID)
 )
 
 CREATE TABLE Orders(
 Order_ID SERIAL NOT NULL,
-User_ID int NOT NULL,
+User_ID int NOT NULL REFERENCES Users(User_ID),
 Status int NOT NULL,
 Order_Date DATE NOT NULL,
 Order_Address varchar(30) NOT NULL,
-PRIMARY KEY (Order_ID),
-FOREIGN KEY (User_ID) REFERENCES Users(User_ID) ON DELETE CASCADE
+PRIMARY KEY (Order_ID)
 )
 
 CREATE TABLE Order_Content(
-Order_ID SERIAL NOT NULL,
-Product_ID int NOT NULL,
+Order_ID SERIAL NOT NULL REFERENCES Orders(Order_ID),
+Product_ID int NOT NULL REFERENCES Product(Product_ID),
 Quantity int NOT NULL,
-PRIMARY KEY (Order_ID, Product_ID),
-FOREIGN KEY (Order_ID) REFERENCES Orders(Order_ID) ON DELETE CASCADE,
-FOREIGN KEY (Product_ID) REFERENCES Product(Product_ID) ON DELETE CASCADE
+PRIMARY KEY (Order_ID, Product_ID)
 )
